107cf877548c8f3eb857f3aa741ab102bcc9da12
banchoban <aleksei.bannikov@gmail.com>
2022-07-05 00:41:29 +0300

    Showing posts by tag

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     # post views
     # re_path(r'^$', views.post_list, name='post_list'),
-    re_path(r'^$', views.PostListView.as_view(), name='post_list'),
+    # re_path(r'^$', views.PostListView.as_view(), name='post_list'),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
     re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
 ]
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,20 +1,30 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+
+from taggit.models import Tag
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
-
+'''
 class PostListView(ListView):
     queryset = Post.objects.filter(status='published')
     context_object_name = 'posts'
     paginate_by = 3
     template_name = './post/list.html'
-
-
 '''
-def post_list(request):
+
+
+def post_list(request, tag_slug=None):
     object_list = Post.objects.all()
+
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -25,8 +35,7 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, './post/list.html', {'page': page, 'posts': posts})
-'''
+    return render(request, './post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
